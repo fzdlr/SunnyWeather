68c868139194bb2c9187d293c3aa0c785191c0cf
zhangyichi <2015315288@qq.com>
2023-03-17 15:22:03 +0800

    add sunny

--- app/src/main/java/com/example/sunnyweather/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/example/sunnyweather/ui/place/PlaceFragment.kt
@@ -1,84 +1,101 @@
 package com.example.sunnyweather.ui.place
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.*
-
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sunnyweather.R
 import com.example.sunnyweather.databinding.FragmentPlaceBinding
 
+private lateinit var recyclerView: RecyclerView
+@SuppressLint("StaticFieldLeak")
+private lateinit var actionBarLayout: FrameLayout
+@SuppressLint("StaticFieldLeak")
+private lateinit var searchPlaceEdit: EditText
+@SuppressLint("StaticFieldLeak")
+private lateinit var bgImageView: ImageView
+@SuppressLint("StaticFieldLeak")
+private lateinit var mRootView: View
 
+
+//控件绑定
+private fun initViews(view: View) {
+    bgImageView = view.findViewById(R.id.bgImageView)
+    searchPlaceEdit = view.findViewById(R.id.searchPlaceEdit)
+    actionBarLayout = view.findViewById(R.id.actionBarLayout)
+    recyclerView = view.findViewById(R.id.recyclerView)
+}
 class PlaceFragment : Fragment() {
-    val binding = FragmentPlaceBinding.inflate(LayoutInflater.from(context))
-    val viewModel by lazy { ViewModelProvider(this)[PlaceViewModel::class.java] }
+    private val viewModel by lazy { ViewModelProvider(this)[PlaceViewModel::class.java] }
     private lateinit var adapter: PlaceAdapter
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_place, container, false)
+        mRootView = inflater.inflate(R.layout.fragment_place, container, false)
+        initViews(mRootView)
+        return mRootView
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-
-        requireActivity().lifecycle.addObserver(object : DefaultLifecycleObserver {
-            @SuppressLint("NotifyDataSetChanged")
-            override fun onCreate(owner: LifecycleOwner) {
-                val layoutManager = LinearLayoutManager(activity)
-                val recyclerView : RecyclerView = binding.root.findViewById(R.id.recyclerView)
-                recyclerView.layoutManager = layoutManager
-                adapter = PlaceAdapter(viewLifecycleOwner as Fragment,viewModel.placeList)
-                recyclerView.adapter = adapter
-                val searchPlaceEdit : EditText = binding.root.findViewById(R.id.searchPlaceEdit)
-                val baImageView : ImageView = binding.root.findViewById(R.id.bgImageView)
-                searchPlaceEdit.addTextChangedListener {
-                     editable -> val content = editable.toString()
-                    if(content.isNotEmpty())
-                    {
-                        viewModel.searchPlaces(content)
-                    }
-                    else
-                    {
-                        recyclerView.visibility = View.GONE
-                        baImageView.visibility = View.VISIBLE
-                        viewModel.placeList.clear()
-                        adapter.notifyDataSetChanged()
-                    }
-                }
-                viewModel.placeLiveData.observe(viewLifecycleOwner, Observer {
-                        result -> val places = result.getOrNull()
-                    if(places!=null)
-                    {
-                        recyclerView.visibility = View.VISIBLE
-                        baImageView.visibility = View.GONE
-                        viewModel.placeList.clear()
-                        viewModel.placeList.addAll(places)
-                        adapter.notifyDataSetChanged()
-                    }
-                    else{
-                        Toast.makeText(activity,"未查询到任何地点",Toast.LENGTH_SHORT).show()
-                        result.exceptionOrNull()?.printStackTrace()
-                    }
-                })
-                owner.lifecycle.removeObserver(this)
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val layoutManager = LinearLayoutManager(activity)
+        recyclerView.layoutManager = layoutManager
+        adapter = PlaceAdapter(this,viewModel.placeList)
+        recyclerView.adapter = adapter
+        Log.d("Fragment","222")
+        searchPlaceEdit.addTextChangedListener {
+            val content = it.toString()
+            Log.d("Fragment","111")
+            if(content.isNotEmpty())
+            {
+                Log.d("Fragment","yes")
+                viewModel.searchPlaces(content)
+            }
+            else
+            {
+                Log.d("Fragment","no")
+               recyclerView.visibility = View.GONE
+                bgImageView.visibility = View.VISIBLE
+                viewModel.placeList.clear()
+                adapter.notifyDataSetChanged()
+            }
+        }
+        Log.d("Fragment","444")
+        viewModel.placeLiveData.observe(viewLifecycleOwner, Observer {
+                result -> Log.d("Fragment","555")
+            val places = result.getOrNull()
+            if(places!=null)
+            {
+                Log.d("Fragment","666")
+                recyclerView.visibility = View.VISIBLE
+                bgImageView.visibility = View.GONE
+                viewModel.placeList.clear()
+                viewModel.placeList.addAll(places)
+                adapter.notifyDataSetChanged()
+            }
+            else{
+                Log.d("Fragment","777")
+                Toast.makeText(activity,"未查询到任何地点",Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace()
             }
         })
-
     }
-
 }
 
 
---
+++ app/src/main/java/com/example/sunnyweather/ui/weather/WeatherActivity.kt
@@ -0,0 +1,108 @@
+package com.example.sunnyweather.ui.weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.TextureView
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.ScrollView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.sunnyweather.R
+import com.example.sunnyweather.databinding.*
+import com.example.sunnyweather.logic.model.Weather
+import com.example.sunnyweather.logic.model.getSky
+import com.example.sunnyweather.logic.network.WeatherService
+import com.example.sunnyweather.ui.place.WeatherViewModel
+import java.text.SimpleDateFormat
+import java.util.*
+class WeatherActivity : AppCompatActivity() {
+
+    private val viewModel by lazy { ViewModelProvider(this)[WeatherViewModel::class.java] }
+    private val forecastLayout: LinearLayout by lazy { findViewById(R.id.forecastLayout) }
+    private val ultravioletText : TextView by lazy { findViewById(R.id.ultravioletText) }
+    private val carWashingText : TextView by lazy { findViewById(R.id.carWashingText) }
+    private val coldRiskText : TextView by lazy { findViewById(R.id.coldRiskText) }
+    private val dressingText : TextView by lazy { findViewById(R.id.dressingText) }
+    private val weatherLayout : ScrollView by lazy { findViewById(R.id.weatherLayout) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+        if(viewModel.LocationLng.isEmpty())
+        {
+            viewModel.LocationLng = intent.getStringExtra("location_lng")?:""
+        }
+        if(viewModel.LocationLat.isEmpty())
+        {
+            viewModel.LocationLat = intent.getStringExtra("location_lat")?:""
+        }
+        if(viewModel.placeName.isEmpty())
+        {
+            viewModel.placeName = intent.getStringExtra("place_name")?:""
+        }
+        viewModel.weatherLiveData.observe(this) { result ->
+            val weather = result.getOrNull()
+            if (weather != null) {
+                showWeatherInfo(weather)
+            } else {
+                Toast.makeText(this, "获取天气信息失败", Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace()
+            }
+        }
+        viewModel.refreshWeather(viewModel.LocationLng,viewModel.LocationLat)
+    }
+
+    private fun showWeatherInfo(weather: Weather)
+    {
+        //now.xml
+
+        val placeName = findViewById<TextView>(R.id.placeName)
+        val currentTemp = findViewById<TextView>(R.id.currentTemp)
+        val currentSky = findViewById<TextView>(R.id.currentSky)
+        val currentAQI = findViewById<TextView>(R.id.currentAQI)
+        val nowLayout = findViewById<RelativeLayout>(R.id.nowLayout)
+
+        placeName.text = viewModel.placeName
+        val realtime = weather.realtime
+        val daily = weather.daily
+        val currentTempText = "${realtime.temperature.toInt()}℃"
+        currentTemp.text = currentTempText
+        currentSky.text = getSky(realtime.skycon).info
+        val currentPM25Text = "空气指数${realtime.airQuality.aqi.chn.toInt()}"
+       currentAQI.text = currentPM25Text
+       nowLayout.setBackgroundResource(getSky(realtime.skycon).bg)
+        //forecast.xml
+       forecastLayout.removeAllViews()
+        val days = daily.skycon.size
+        for(i in 0 until days )
+        {
+            val view = LayoutInflater.from(this).inflate(R.layout.forecast_item,forecastLayout,false)
+            val dateInfo = view.findViewById<TextView>(R.id.dateInfo)
+            val skyInfo = view.findViewById<TextView>(R.id.skyInfo)
+            val temperatureInfo = view.findViewById<TextView>(R.id.temperatureInfo)
+            val skyIcon = view.findViewById<ImageView>(R.id.skyIcon)
+            val sky = daily.skycon[i]
+            val temperature = daily.temperature[i]
+            val simpleformat = SimpleDateFormat("yyyy-mm-dd", Locale.getDefault())
+            dateInfo.text = simpleformat.format(sky.date)
+            val skycon = getSky(sky.value)
+            skyInfo.text = skycon.info
+            val tempText = "${temperature.min.toInt()}~${temperature.max.toInt()}℃"
+            temperatureInfo.text = tempText
+            skyIcon.setImageResource(skycon.icon)
+            forecastLayout.addView(view)
+        }
+        //lifeindex.xml
+        val lifeIndex = daily.lifeIndex
+        coldRiskText.text = lifeIndex.coldRisk[0].desc
+        carWashingText.text = lifeIndex.carWashing[0].desc
+        ultravioletText.text = lifeIndex.ultraviolet[0].desc
+        dressingText.text = lifeIndex.dressing[0].desc
+        weatherLayout.visibility = View.VISIBLE
+    }
+}
